80e1d0fdb5cec9fcd9d1eb989dbce2238feb0d95
tungpv <tungpv@mirabo-global.com>
2025-05-12 21:35:08 +0700

    feat: register user

--- app/src/main/java/com/example/expensemanager/api/AuthService.java
+++ app/src/main/java/com/example/expensemanager/api/AuthService.java
@@ -1,6 +1,8 @@
 package com.example.expensemanager.api;
 import com.example.expensemanager.model.LoginRequest;
 import com.example.expensemanager.model.LoginResponse;
+import com.example.expensemanager.model.RegisterRequest;
+import com.example.expensemanager.model.RegisterResponse;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
@@ -9,4 +11,7 @@ import retrofit2.http.POST;
 public interface AuthService {
     @POST("/auth/login")
     Call<LoginResponse> login(@Body LoginRequest request);
+
+    @POST("auth/register")
+    Call<RegisterResponse> register(@Body RegisterRequest request);
 }
--- app/src/main/java/com/example/expensemanager/model/RegisterResponse.java
+++ app/src/main/java/com/example/expensemanager/model/RegisterResponse.java
@@ -1,4 +1,20 @@
 package com.example.expensemanager.model;
 
 public class RegisterResponse {
+    private String message;
+    private User user;
+    private String accessToken;
+    private String refreshToken;
+
+    public String getMessage() { return message; }
+    public void setMessage(String message) { this.message = message; }
+
+    public User getUser() { return user; }
+    public void setUser(User user) { this.user = user; }
+
+    public String getAccessToken() { return accessToken; }
+    public void setAccessToken(String accessToken) { this.accessToken = accessToken; }
+
+    public String getRefreshToken() { return refreshToken; }
+    public void setRefreshToken(String refreshToken) { this.refreshToken = refreshToken; }
 }
